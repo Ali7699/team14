180e602ff3f7439f87518441428d2be0113bfdb5
Ali7699 <ali.m.mahmoud48@gmail.com>
2025-04-02 18:01:23 +0200

    fixed cancel in x waitlist

--- Team14/Scheduler.h
+++ Team14/Scheduler.h
@@ -29,23 +29,44 @@ private:
 	//other data members 
 	
 	int timeStep;
-
+	int totalPatients;
+	int finishedPatients;
 
 public:
+	/*
+	Function notation
+
+	Phase 1: This Function is only implemented for phase 1
+	Phase 2: ""
+	Polymorphic: this function is implemented in both phases, but will have a diffrent implemnation in phase 2
+	Universal: This function stays the same in both phases
 	
-	//constructor
+	
+	*/
+
+
+
+
+
+	//universal: constructor
 	Scheduler();
 	//destructor
 	~Scheduler();
 
-	//1.Intializer
+	//1.Intializer:Universal
 	void loadInputFile();
 
 
-	//2.Main function, Contains simulation while loop, Calls all helper functions
+	//2.Main function (Contains simulation while loop, Calls all helper functions) : Polymorphic 
 	void Simulation();
 	
 
 	//3.List movers
-	
+
+	//moves from All :Universal(always dependant on PT and VT)
+	bool moveAll(); 
+
+
+
+
 };
--- Team14/X_Waitlist.h
+++ Team14/X_Waitlist.h
@@ -3,7 +3,10 @@
 
 class X_Waitlist:public EU_Waitlist {
 public:
-	bool cancel() {
+	
+	//this function is responsible for 1.determining if an eligible patients exists to cancel 2.choosing one of them to deque.
+	//it is NOT responiosble for when its called, might be called when list is empty so naturally no patient return
+	bool cancel(Patient*&outpatient) {
 		//intilizations
 		Node<Patient*>*current = frontPtr;
 		Patient* Arr[100];
@@ -16,7 +19,11 @@ public:
 		//first, identify eligible patients
 		while (current!= nullptr) {
 			Patient*TempPatient=current->getItem();
+			
+			//getTreatmentList returns a copy, so we are free to abuse TempTreatmentList
 			LinkedQueue<Treatment*> TempTreatmentList=TempPatient->getTreatmentlist();
+			
+			
 			int treatmentcount=0;
 			bool Xchecker=false;
 
@@ -57,34 +64,32 @@ public:
 		
 		//now finally find The Chosenpatient and remove him
 
-		//Case A: Patient is first on the list
+		Node<Patient*>* prev = nullptr;
 		current = frontPtr;
-		if (randomindex == 0) {
-			
-			frontPtr = current->getNext();
-			
-			//edge case for single node list, we need to update back ptr
-			if (frontPtr == nullptr) {  
-				backPtr = nullptr;      
-			}
-			delete current;
-			return true;
-		}
-		
-		//Case B: patient is anywhere else
-		while (current!= nullptr&&current->getNext()!=nullptr) {
-			
-			if (current->getNext()->getItem() == ChosenPatient) {
-				Node<Patient*>* prev = current;
-				current = current->getNext();
-				prev->setNext(current->getNext());
-
+		while (current != nullptr) {
+			if (current->getItem() == ChosenPatient) {
+				if (prev == nullptr) {
+					//prev only is ever nullptr first run, therefore Patient is in first node
+					frontPtr = current->getNext();
+					if (frontPtr == nullptr) {
+						backPtr = nullptr; // List is now empty
+					}
+				}
+				else {
+					prev->setNext(current->getNext());
+					// Update backPtr if removing the last node
+					if (current->getNext() == nullptr) {
+						backPtr = prev;
+					}
+				}
+				outpatient = current->getItem();
 				delete current;
 				return true;
-
 			}
+			prev = current;
 			current = current->getNext();
 		}
+
 		return false;
 	}
 	
